d04ab82048113252220b3df661ffb51c293f7642
Marysia Pabich <marysia.pabich@gmail.com>
2020-03-20 21:19:32 +0100

    Get posts from redux store, use bootstrap cols to adjust to responsive

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import CardGroup from 'react-bootstrap/CardGroup';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
 import { connect } from 'react-redux';
 import { getUser } from '../../../redux/userRedux.js';
+import { getAll } from '../../../redux/postsRedux.js';
 
 import clsx from 'clsx';
 
@@ -14,27 +16,28 @@ import clsx from 'clsx';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
 import styles from './Homepage.module.scss';
-import { myads } from '../../../db';
 
-const Component = ({ className, user }) => (
+const Component = ({ className, user, posts }) => (
   <div className={clsx(className, styles.root)}>
     <h3>My ads</h3>
-    <CardGroup>
-      {myads.map(ad => (
-        <Card {...ad} className={styles.ad} key={ad.id}>
-          <Card.Img className={styles.cardImage} variant="top" src={ad.image} />
-          <Card.Body>
-            <Card.Title><a href={`/post/:${ad.id}`}>{ad.title}</a></Card.Title>
-            <Card.Text>
-              {ad.location}
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Published {ad.publishedDate}</small>
-          </Card.Footer>
-        </Card>
+    <Row>
+      {posts.map(ad => (
+        <Col xs={12} md={6} lg={4} key={ad.id}>
+          <Card {...ad} className={styles.ad}>
+            <Card.Img className={styles.cardImage} variant="top" src={ad.image} />
+            <Card.Body>
+              <Card.Title><a href={`/post/:${ad.id}`}>{ad.title}</a></Card.Title>
+              <Card.Text>
+                {ad.location}
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Published {ad.publishedDate}</small>
+            </Card.Footer>
+          </Card>
+        </Col>
       ))}
-    </CardGroup>
+    </Row>
     {user.authenticated ? (
       <Button href="/post/add" variant="dark">Add new post</Button>
     ) : ''}
@@ -44,10 +47,12 @@ const Component = ({ className, user }) => (
 Component.propTypes = {
   className: PropTypes.string,
   user: PropTypes.object,
+  posts: PropTypes.array,
 };
 
 const mapStateToProps = state => ({
   user: getUser(state),
+  posts: getAll(state),
 });
 
 // const mapDispatchToProps = dispatch => ({
