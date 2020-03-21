dfd6af23befdc39c28d959eac192774e4d85137c
Marysia Pabich <marysia.pabich@gmail.com>
2020-03-21 12:27:39 +0100

    Make componenent statefull, add onChange function for inputs, prepare submitForm func

--- src/components/views/PostAdd/PostAdd.js
+++ src/components/views/PostAdd/PostAdd.js
@@ -7,85 +7,121 @@ import Col from 'react-bootstrap/Col';
 
 import clsx from 'clsx';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+import { connect } from 'react-redux';
+import { addPost } from '../../../redux/postsRedux.js';
 
 import styles from './PostAdd.module.scss';
 
-const Component = ({ className }) => (
-  <div className={clsx(className, styles.root)}>
-    <h2>PostAdd</h2>
-    <Form>
-      <Form.Row>
-        <Col lg={8}>
-          <Form.Group controlId="postTitle">
-            <Form.Label>Title</Form.Label>
-            <Form.Control minLength="10" required type="text" placeholder="Enter title" />
-            <Form.Text className="text-muted">
-              Use catching keywords
-            </Form.Text>
-          </Form.Group>
-        </Col>
-        <Col lg={4}>
-          <Form.Group controlId="epostEmail">
-            <Form.Label>E-mail</Form.Label>
-            <Form.Control required type="email" placeholder="name@example.com" />
-            <Form.Text className="text-muted">
-              Enter your e-mail address to let people contact you easily
-            </Form.Text>
-          </Form.Group>
-        </Col>
-      </Form.Row>
-      <Form.Group controlId="postContent">
-        <Form.Label>Describe the object or matter of your post</Form.Label>
-        <Form.Control minLength="20" required as="textarea" rows="3" />
-        <Form.Text className="text-muted">
-          Describe the object or matter of your post
-        </Form.Text>
-      </Form.Group>
-      <Form.Row>
-        <Col sm={12} md={4}>
-          <Form.Group controlId="postLocation">
-            <Form.Label>Location</Form.Label>
-            <Form.Control required type="text" placeholder="Enter your location" />
-          </Form.Group>
-        </Col>
-        <Col sm={12} md={4}>
-          <Form.Group controlId="postPrice">
-            <Form.Label>Price</Form.Label>
-            <Form.Control required type="text" placeholder="Enter price" />
-          </Form.Group>
-        </Col>
-        <Col sm={12} md={4}>
-          <Form.Group controlId="postPhone">
-            <Form.Label>Phone</Form.Label>
-            <Form.Control required type="text" placeholder="Enter telephone number" />
-          </Form.Group>
-        </Col>
-      </Form.Row>
-      <Button variant="dark" type="submit">
-        Add Post
-      </Button>
-    </Form>
-  </div>
-);
+class Component extends React.Component {
 
-Component.propTypes = {
-  className: PropTypes.string,
-};
+  state = {
+    postData: {
+      id: '',
+      title: '',
+      content: '',
+      email: '',
+      location: '',
+      published: '',
+      updated: '',
+      phone: '',
+      price: '',
+      image: '',
+    },
+  }
+
+  static propTypes = {
+    className: PropTypes.string,
+    addPost: PropTypes.func,
+  }
+
+  updateInputValue = ({ target }) => {
+    const { postData } = this.state;
+    const { value, name } = target;
+
+    this.setState({ postData: { ...postData, [name]: value } });
+  };
+
+  submitPost = e => {
+    e.preventDefault();
+  };
+
+  render() {
+    const { updateInputValue, submitPost } = this;
+    const { postData } = this.state;
+    const { className } = this.props;
+
+    return (
+      <div className={clsx(className, styles.root)}>
+        <h2>PostAdd</h2>
+        <Form onSubmit={submitPost}>
+          <Form.Row>
+            <Col lg={8}>
+              <Form.Group controlId="postTitle">
+                <Form.Label>Title</Form.Label>
+                <Form.Control name="title" onChange={updateInputValue} value={postData.title} minLength="10" required type="text" placeholder="Enter title" />
+                <Form.Text className="text-muted">
+                  Use catching keywords
+                </Form.Text>
+              </Form.Group>
+            </Col>
+            <Col lg={4}>
+              <Form.Group controlId="epostEmail">
+                <Form.Label>E-mail</Form.Label>
+                <Form.Control name="email" onChange={updateInputValue} value={postData.email} required type="email" placeholder="name@example.com" />
+                <Form.Text className="text-muted">
+                  Enter your e-mail address to let people contact you easily
+                </Form.Text>
+              </Form.Group>
+            </Col>
+          </Form.Row>
+          <Form.Group controlId="postContent">
+            <Form.Label>Describe the object or matter of your post</Form.Label>
+            <Form.Control name="content" onChange={updateInputValue} value={postData.content} minLength="20" required as="textarea" rows="3" />
+            <Form.Text className="text-muted">
+              Describe the object or matter of your post
+            </Form.Text>
+          </Form.Group>
+          <Form.Row>
+            <Col sm={12} md={4}>
+              <Form.Group controlId="postLocation">
+                <Form.Label>Location</Form.Label>
+                <Form.Control name="location" onChange={updateInputValue} value={postData.location} required type="text" placeholder="Enter your location" />
+              </Form.Group>
+            </Col>
+            <Col sm={12} md={4}>
+              <Form.Group controlId="postPrice">
+                <Form.Label>Price</Form.Label>
+                <Form.Control name="price" onChange={updateInputValue} value={postData.price} required type="text" placeholder="Enter price" />
+              </Form.Group>
+            </Col>
+            <Col sm={12} md={4}>
+              <Form.Group controlId="postPhone">
+                <Form.Label>Phone</Form.Label>
+                <Form.Control name="phone" onChange={updateInputValue} value={postData.phone} required type="text" placeholder="Enter telephone number" />
+              </Form.Group>
+            </Col>
+          </Form.Row>
+          <Button variant="dark" type="submit">
+            Add Post
+          </Button>
+        </Form>
+      </div >
+    );
+  }
+}
 
 // const mapStateToProps = state => ({
 //   someProp: reduxSelector(state),
 // });
 
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
+const mapDispatchToProps = dispatch => ({
+  addPost: post => dispatch(addPost(post)),
+});
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(null, mapDispatchToProps)(Component);
 
 export {
-  Component as PostAdd,
-  // Container as PostAdd,
+  // Component as PostAdd,
+  Container as PostAdd,
   Component as PostAddComponent,
 };
