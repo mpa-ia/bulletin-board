da30070c681266c4c833b82f695a059658e5e629
Marysia Pabich <marysia.pabich@gmail.com>
2020-03-24 23:17:53 +0100

    Add enpoint to get post by id

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -3,8 +3,19 @@ const Post = require('../models/post.model');
 exports.loadAll = async (req, res) => {
   try {
     const posts = await Post.find({ status: 'Published' });
-    if (!posts) res.status(404).json({ post: 'Not Found' })
+    if (!posts) res.status(404).json({ post: 'Not Found' });
     else res.json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.loadPostById = async (req, res) => {
+  try {
+    console.log(req.params);
+    const post = await Post.find({ _id: req.params.id });
+    if (!post) res.status(404).json({ post: 'Not Found' });
+    else res.json(post);
   } catch (err) {
     res.status(500).json(err);
   }
