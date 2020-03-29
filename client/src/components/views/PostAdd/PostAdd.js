8b0810200f5e531eb18dcee8b971edf5d92b8472
Marysia Pabich <marysia.pabich@gmail.com>
2020-03-29 15:50:50 +0200

    Code refactor

--- client/src/components/views/PostAdd/PostAdd.js
+++ client/src/components/views/PostAdd/PostAdd.js
@@ -87,7 +87,14 @@ class Component extends React.Component {
               <Col md={8}>
                 <Form.Group controlId="postTitle">
                   <Form.Label>Title</Form.Label>
-                  <Form.Control name="title" onChange={updateInputValue} value={postData.title} minLength="10" required type="text" placeholder="Enter title" />
+                  <Form.Control
+                    name="title"
+                    onChange={updateInputValue}
+                    value={postData.title}
+                    minLength="10" required
+                    type="text"
+                    placeholder="Enter title"
+                  />
                   <Form.Text className="text-muted">
                     Use catching keywords
                   </Form.Text>
@@ -96,7 +103,11 @@ class Component extends React.Component {
               <Col md={4}>
                 <Form.Group controlId="postStatus">
                   <Form.Label>Status</Form.Label>
-                  <Form.Control name="status" defaultValue="Draft" readOnly type="text" />
+                  <Form.Control
+                    name="status"
+                    defaultValue="Draft"
+                    readOnly
+                    type="text" />
                 </Form.Group>
               </Col>
             </Form.Row>
@@ -104,7 +115,13 @@ class Component extends React.Component {
               <Col md={6}>
                 <Form.Group controlId="epostEmail">
                   <Form.Label>E-mail</Form.Label>
-                  <Form.Control name="email" onChange={updateInputValue} value={postData.email} required type="email" placeholder="name@example.com" />
+                  <Form.Control
+                    name="email"
+                    onChange={updateInputValue}
+                    value={postData.email}
+                    required
+                    type="email"
+                    placeholder="name@example.com" />
                   <Form.Text className="text-muted">
                     Enter your e-mail address to let people contact you easily
                   </Form.Text>
@@ -113,7 +130,13 @@ class Component extends React.Component {
               <Col md={6}>
                 <Form.Group controlId="postPhone">
                   <Form.Label>Phone</Form.Label>
-                  <Form.Control name="phone" onChange={updateInputValue} value={postData.phone} type="text" placeholder="Enter telephone number" />
+                  <Form.Control
+                    name="phone"
+                    onChange={updateInputValue}
+                    value={postData.phone}
+                    type="text"
+                    placeholder="Enter telephone number"
+                  />
                   <Form.Text className="text-muted">
                     Entering phone is optional, but makes reaching you easy
                   </Form.Text>
@@ -122,7 +145,15 @@ class Component extends React.Component {
             </Form.Row>
             <Form.Group controlId="postContent">
               <Form.Label>Post description</Form.Label>
-              <Form.Control name="content" onChange={updateInputValue} value={postData.content} minLength="20" required as="textarea" rows="3" />
+              <Form.Control
+                name="content"
+                onChange={updateInputValue}
+                value={postData.content}
+                minLength="20"
+                required
+                as="textarea"
+                rows="5"
+              />
               <Form.Text className="text-muted">
                 Describe the object or matter of your post
               </Form.Text>
@@ -131,17 +162,33 @@ class Component extends React.Component {
               <Col sm={12} md={4}>
                 <Form.Group controlId="postLocation">
                   <Form.Label>Location</Form.Label>
-                  <Form.Control name="location" onChange={updateInputValue} value={postData.location} type="text" placeholder="Enter your location" />
+                  <Form.Control
+                    name="location"
+                    onChange={updateInputValue}
+                    value={postData.location}
+                    type="text"
+                    placeholder="Enter your location"
+                  />
                 </Form.Group>
               </Col>
               <Col sm={12} md={4}>
                 <Form.Group controlId="postPrice">
                   <Form.Label>Price</Form.Label>
-                  <Form.Control name="price" onChange={updateInputValue} value={postData.price} type="text" placeholder="Enter price" />
+                  <Form.Control
+                    name="price"
+                    onChange={updateInputValue}
+                    value={postData.price}
+                    type="text"
+                    placeholder="Enter price"
+                  />
                 </Form.Group>
               </Col>
             </Form.Row>
-            <input name="image" onChange={setImage} type="file" />
+            <input
+              name="image"
+              onChange={setImage}
+              type="file"
+            />
             <Button
               type="submit"
               startIcon={<AddIcon />}
--- client/src/components/views/PostEdit/PostEdit.js
+++ client/src/components/views/PostEdit/PostEdit.js
@@ -82,7 +82,14 @@ class Component extends React.Component {
               <Col md={8}>
                 <Form.Group controlId="postTitle">
                   <Form.Label>Title</Form.Label>
-                  <Form.Control name="title" onChange={updateInputValue} value={postData.title} minLength="10" required type="text" placeholder="Enter title" />
+                  <Form.Control
+                    name="title"
+                    onChange={updateInputValue}
+                    value={postData.title}
+                    minLength="10"
+                    required type="text"
+                    placeholder="Enter title"
+                  />
                   <Form.Text className="text-muted">
                     Use catching keywords
                   </Form.Text>
@@ -91,16 +98,27 @@ class Component extends React.Component {
               <Col md={4}>
                 <Form.Group controlId="postStatus">
                   <Form.Label>Status</Form.Label>
-                  <Form.Control name="status" defaultValue={post.status} readOnly type="text" />
+                  <Form.Control
+                    name="status"
+                    defaultValue={post.status}
+                    readOnly
+                    type="text"
+                  />
                 </Form.Group>
               </Col>
             </Form.Row>
             <Form.Row>
-
               <Col md={6}>
                 <Form.Group controlId="postEmail">
                   <Form.Label>E-mail</Form.Label>
-                  <Form.Control name="email" onChange={updateInputValue} value={postData.email} required type="email" placeholder="name@example.com" />
+                  <Form.Control
+                    name="email"
+                    onChange={updateInputValue}
+                    value={postData.email}
+                    required
+                    type="email"
+                    placeholder="name@example.com"
+                  />
                   <Form.Text className="text-muted">
                     Enter your e-mail address to let people contact you easily
                   </Form.Text>
@@ -109,7 +127,12 @@ class Component extends React.Component {
               <Col md={6}>
                 <Form.Group controlId="postPhone">
                   <Form.Label>Phone</Form.Label>
-                  <Form.Control name="phone" onChange={updateInputValue} value={postData.phone} required type="text" />
+                  <Form.Control
+                    name="phone"
+                    onChange={updateInputValue}
+                    value={postData.phone}
+                    type="text"
+                  />
                   <Form.Text className="text-muted">
                     Entering phone is optional, but makes reaching you easy
                   </Form.Text>
@@ -118,7 +141,13 @@ class Component extends React.Component {
             </Form.Row>
             <Form.Group controlId="postContent">
               <Form.Label>Post description</Form.Label>
-              <Form.Control name="content" onChange={updateInputValue} value={postData.content} minLength="20" required as="textarea" rows="3" />
+              <Form.Control name="content"
+                onChange={updateInputValue}
+                value={postData.content}
+                minLength="20"
+                required as="textarea"
+                rows="5"
+              />
               <Form.Text className="text-muted">
                 Describe the object or matter of your post
               </Form.Text>
@@ -127,18 +156,31 @@ class Component extends React.Component {
               <Col sm={12} md={4}>
                 <Form.Group controlId="postLocation">
                   <Form.Label>Location</Form.Label>
-                  <Form.Control name="location" onChange={updateInputValue} value={postData.location} required type="text" />
+                  <Form.Control
+                    name="location"
+                    onChange={updateInputValue}
+                    value={postData.location}
+                    type="text"
+                  />
                 </Form.Group>
               </Col>
               <Col sm={12} md={4}>
                 <Form.Group controlId="postPrice">
                   <Form.Label>Price</Form.Label>
-                  <Form.Control name="price" onChange={updateInputValue} value={postData.price} required type="text" />
+                  <Form.Control
+                    name="price"
+                    onChange={updateInputValue}
+                    value={postData.price}
+                    type="text"
+                  />
                 </Form.Group>
               </Col>
-
             </Form.Row>
-            <input name="image" onChange={setImage} type="file" />
+            <input
+              name="image"
+              onChange={setImage}
+              type="file"
+            />
             <Button
               type="submit"
               startIcon={<TrendingUpIcon />}
